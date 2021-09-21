befd164979e9e39b837a4601e471107d8ec145a7
Gabriel L Calsada <gabriellcalsada@gmail.com>
2021-09-21 20:13:23 -0300

    Add. tela de alerta e readme.md.

--- src/screens/alertReport/index.tsx
+++ src/screens/alertReport/index.tsx
@@ -1,11 +1,101 @@
 import React from 'react';
-import { Stack, Center, Image, Box } from 'native-base';
+import { Stack, Center, Image, Box, Text } from 'native-base';
 import { Header } from '../../components/Header';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ScrollView } from 'react-native';
+import { number } from 'yargs';
+
+interface Coordinate {
+  latitude: number;
+  longitude: number;
+}
+
+interface DriverAlert {
+  id: number;
+  eventDateTime: string;
+  coordinates: Coordinate;
+  generatedScore: number;
+  eventType: number;
+  eventTypeDescription: string;
+  precisionLevel: number;
+}
+
+function formatAlertDateTime(date: Date) {
+  return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+}
+
+function translateEventDescription(eventTypeDescription: string) {
+  switch (eventTypeDescription) {
+    case 'TRIP_COMPLETE':
+      return 'Viagem completa';
+    
+    case 'ACCIDENT':
+      return 'Acidente';
+    
+    case 'SUDDEN_BREAK':
+      return 'Freada brusca';
+    
+    case 'OVER_VELOCITY':
+      return 'Acima da velocidade';
+        
+    case 'CELLPHONE_USAGE':
+      return 'Uso do celular';
+    
+    case 'ILLEGAL_TURN':
+      return 'Virada ilegal';
+      
+    default:
+      return '';
+      break;
+  }
+}
+
 export function alertReport() {
+
+  const [alertState, setAlertState] = useState<DriverAlert[]>([{}]);
+  
+  const api = axios.create({
+    baseURL: ''
+  });
+
+  useEffect(() => {
+    api
+    .get(`https://safe-driver-api.herokuapp.com/api/DriverEvent/9ac3a29c-a0e8-4107-a540-005bdab6a7ac`)
+    .then((response) => {
+      setAlertState(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+
+  }, [])
+
   return (
     <>
       <Header title="Relatório de alertas" />
-      <Stack alignItems="center" mt={10}></Stack>
+      <ScrollView>
+        <Stack alignItems="flex-start" m={6}>
+          {
+            alertState.map((alertData) => (
+              <Stack
+              padding={3}
+              bg="#3575BE"
+              rounded="md"
+              direction="column"
+              space={2}
+              mb={2}
+              width="100%"
+            >
+              <Text color="white">{"Registrado em: " + formatAlertDateTime(new Date(alertData.eventDateTime))}</Text>
+              <Text color="white">{translateEventDescription(alertData.eventTypeDescription)}</Text>
+              <Text color="white">{"Score: " + alertData.generatedScore}</Text>
+            </Stack>
+            ))
+          }
+        </Stack>
+
+      </ScrollView>
     </>
   );
 }
